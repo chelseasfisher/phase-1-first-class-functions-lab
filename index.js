21ee633f1a2eafa5fdbd002e730be3868b20c6ca
Chelsea Fisher <chelseasfisher1@gmail.com>
2021-12-29 20:20:04 -0600

    commit tree 6e151a0b0b77eacf8f0fdea1fd51cdc5a7cc2fee

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return [ 'Antonia', 'Nuru' ];
+};
+
+const returnLastTwoDrivers = function (drivers){
+    return [ 'Amari', 'Mo' ];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiply) {
+    return function fareQuintupler (value){
+        return multiply * value;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers,driveorder){
+    return driveorder(drivers);
+}
